3a58a4e129c16981d8987d6dd01fa482c6ae3f53
ciro <engima1923@gamil.com>
2021-11-17 23:30:53 +0800

    Update

--- config.js
+++ config.js
@@ -56,22 +56,22 @@ module.exports = {
 
     if (process.env.NODE_ENV === 'production') {
       publish = def.concat(
-        // index.HtmlWebpackPlugin,
-        // sports.HtmlWebpackPlugin,
-        // liveCasion.HtmlWebpackPlugin,
-        // promotions.HtmlWebpackPlugin,
-        // virtualSports.HtmlWebpackPlugin,
-        // games.HtmlWebpackPlugin,
-        // deposit.HtmlWebpackPlugin,
-        // statement.HtmlWebpackPlugin,
-        // contact.HtmlWebpackPlugin,
-        accountAccount.HtmlWebpackPlugin,
-        accountBalance.HtmlWebpackPlugin,
-        accountBets.HtmlWebpackPlugin,
-        accountForm.HtmlWebpackPlugin,
-        accountPeofile.HtmlWebpackPlugin,
-        accountPromotion.HtmlWebpackPlugin,
-        accountReferral.HtmlWebpackPlugin
+        index.HtmlWebpackPlugin,
+        sports.HtmlWebpackPlugin,
+        liveCasion.HtmlWebpackPlugin,
+        promotions.HtmlWebpackPlugin,
+        virtualSports.HtmlWebpackPlugin,
+        games.HtmlWebpackPlugin,
+        deposit.HtmlWebpackPlugin,
+        statement.HtmlWebpackPlugin,
+        contact.HtmlWebpackPlugin
+        // accountAccount.HtmlWebpackPlugin,
+        // accountBalance.HtmlWebpackPlugin,
+        // accountBets.HtmlWebpackPlugin,
+        // accountForm.HtmlWebpackPlugin,
+        // accountPeofile.HtmlWebpackPlugin,
+        // accountPromotion.HtmlWebpackPlugin,
+        // accountReferral.HtmlWebpackPlugin
       );
     }
 
